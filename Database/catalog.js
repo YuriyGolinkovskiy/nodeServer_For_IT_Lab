feddec30f50e80d73e118e9da75358f109698804
YuriyGolinkovskiy <45183992+YuriyGolinkovskiy@users.noreply.github.com>
2021-11-29 12:49:14 +0300

    Delete Database directory

--- Database/catalog.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const Catalog = new mongoose.Schema({
-    title: { type: String, required: true },
-    url: { type: String, required: true },
-    products: { type: Array },
-});
-
-export default mongoose.model('Catalog', Catalog);
